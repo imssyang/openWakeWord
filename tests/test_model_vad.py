d0365bddde84b76284b9530738a2cb6b5042c9db
imssyang <imssyang@gmail.com>
2026-02-02 17:53:40 +0800

    update vad

--- tests/test_model_vad.py
+++ tests/test_model_vad.py
@@ -4,6 +4,7 @@ import numpy as np
 import sounddevice as sd
 import torch
 from source.utils import AudioPlayer, AudioFile, FilePlayer, MicPlayer
+from source.models import SileroVadModel
 
 
 work_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -43,4 +44,36 @@ def test_vad_torch():
     plt.ylabel("Confidence")
     plt.savefig(f"{work_dir}/tests/test_vad_output.png")
     plt.close()
+
+
+def test_onnx():
+    path = f"{work_dir}/models/onnx/silero_vad.onnx"
+    model = SileroVadModel(path)
+    print(f"Loaded model: {path} {model}")
+
+    confidences = []
+    samplerate = 16000
+    chunks = AudioFile(f"{work_dir}/data/vad/test.wav").get_chunks(
+        chunksize=512, samplerate=samplerate, enable_mono=True, dtype='float32')
+    for i, chunk in enumerate(chunks):
+        chunk = chunk.flatten()  # shape: (512,)
+
+        with torch.no_grad():
+            confidence = model(
+                torch.from_numpy(chunk),
+                samplerate,
+            ).item()
+
+        confidences.append(confidence)
+
+    print("Stopped Recording")
+    plt.figure(figsize=(20, 6))
+    plt.plot(confidences)
+    plt.title("Voiced Confidence")
+    plt.xlabel("Frame")
+    plt.ylabel("Confidence")
+    plt.savefig(f"{work_dir}/tests/test_vad_output.png")
+    plt.close()
+
+
 
